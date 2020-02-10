8b6ab02b33532d9f2ffb17f798623779e8c31e53
Maurita Brogg <mbrogg@il-h3.21-school.ru>
2020-02-10 21:31:29 +0300

    A t_dbl t_ldbl .ignore

--- fl_main.c
+++ fl_main.c
@@ -3,10 +3,13 @@
 
 int main()
 {
-    double test = 1.5;
-    long double test1 = 3.125;
+    double test = 1.25;
+    long double test1 = 1.25;
 
     printf("std %f\n", test);
     ft_printf("our %f\n", test);
+
+    // printf("\nstd %Lf\n", test1);
+    // ft_printf("our %Lf\n", test1);
     return (0);
 }
--- srcs/floats.c
+++ srcs/floats.c
@@ -49,19 +49,47 @@ void    print_dbl(t_dbl to_print)
     printf("\n");
 }
 
+unsigned    exp_shift(unsigned exp, unsigned *shift, unsigned dbl_ldbl)
+{
+    if (dbl_ldbl == 1)
+    {
+        *shift = exp - 1023;
+        return (*shift > 0 ? 1 : -1);
+    }
+    else if (dbl_ldbl == 2)
+    {
+        *shift = exp - 1023; // 1023?
+        return (*shift > 0 ? 1 : -1);
+    }
+}
+
 void    print_float(t_lst *temp, double input)
 {
     t_dbl   res;
+    unsigned    shift;
 
     res.origin = input;
     printf("%u\n", res.parts.sign);
     printf("%u\n", res.parts.exp);
     printf("%llu\n", res.parts.mant);
+    printf("\nMANT_BINARY\n%s\n", ft_itoa_base(res.parts.mant, 2));
+    printf("%s\n", ft_itoa_base(res.parts.exp, 2));
+    // print_bit(res.parts.exp, 11);
+
+    // if (exp_shift(res.parts.exp, &shift, 1) > 0) ;
+        
 }
 
 void    print_lfloat(t_lst *temp, long double input)
 {
     t_ldbl  res;
+    unsigned    shift;
 
     res.origin = input;
+    printf("%u\n", res.parts.sign);
+    printf("%u\n", res.parts.exp);
+    printf("%llu\n", res.parts.mant);
+    printf("\nMANT_BINARY\n%s\n", ft_itoa_base(res.parts.mant, 2));
+
+    // if (exp_shift(res.parts.exp, &shift, 2) > 0) ;
 }
